e8884f7422378903761021bdcedf147f31bfe007
Syedayyan168 <syedayyanalimadni@gmail.com>
2024-12-18 08:32:54 -0800

    Add files via upload

---
+++ assigment 1/app.js
@@ -0,0 +1,106 @@
+//                       CHAPTER : NO : 1
+// QUESTION 1
+
+alert("welcome! SMIT");
+
+// QUESTION 2
+
+alert("Error! Please Enter a correct password.");
+
+// QUESTION 3
+
+alert("Welcome to JS Land \nHard Coding For SMIT!");
+
+// QUESTION 4
+
+alert("Welcome to JS Land");
+alert("Hard Coding!");
+
+// QUESTION 5
+
+console.log("Hello... I can run JS through my web browser's console");
+
+                       const str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+                       alert(str);
+
+
+//                       CHAPTER :NO: 2
+
+// QUESTION 1
+
+ const username = "ayyan-n-ali"
+ alert (username);
+
+// QUESTION 2
+
+const myname = "Syed Ayyan Ali Madni";
+   alert(myname);
+
+// QUESTION 3
+
+const message = "SAYLANI . SMIT!";
+alert(message);
+
+
+// QUESTION 4
+
+const studentName = "Mohammad Hassan Numan!";
+const studentAge = 17;
+const studentCourse = "web and mobile app development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// QUESTION 5
+
+const display = "Burger \nBurge \nBurg \nBur \nBU \nB";
+alert(display);
+
+
+// QUESTION 6
+
+const email = "syedayyanalimadni@gmail.com";
+alert(email);
+
+// QUESTION 7
+
+const book = "Codecademy ki JavaScript course bahut hi popular hai aur naye learners ke liye bahut hi upyukt hai.";
+alert(book);
+
+// QUESTION 8
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+           
+             const ayn = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+             alert(ayn);
+
+                 //    CHAPTER :NO: 3
+
+// QUESTION 1
+
+const age = 17;
+alert(age);
+
+// QUESTION 2
+
+// QUESTION 3
+
+const birthYear = 2008;
+document.write("My birth year is " + birthYear);
+document.write("<br/> Data type of declared variable is number.");
+
+
+// QUESTION 4
+
+const visitorName = "Syed Anwer Ali";
+const productTitle = "10  books";
+const quantity = 2;
+document.write(visitorName + " ordered " + productTitle + " on Ayyan books store.");
+
+
+                       const ayy = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+                       alert(ayy);
+
+                       const end = "THE END";
+                       alert(end);
